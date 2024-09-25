5750113a1a769e6c6705d7353ebae3a0e1cac859
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2024-09-25 18:02:08 +0100

    review

--- src/urlLoader.js
+++ src/urlLoader.js
@@ -1,5 +1,5 @@
  module.exports = { 
     // url: "http://localhost:4500"
-    url: "https://10c224bbe5679d070e7e6bf31bf8153a.serveo.net"
+    url: "https://2f0686f925209d51d1d3ba87e92c5b0b.serveo.net"
 }; 
  
