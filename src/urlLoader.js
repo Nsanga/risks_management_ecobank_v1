06bb8aaf9590d137ba85abc2734e805f90eb807d
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2024-09-26 20:00:44 +0100

    review

--- src/urlLoader.js
+++ src/urlLoader.js
@@ -1,5 +1,5 @@
  module.exports = { 
-    // url: "http://localhost:4500"
-    url: "https://828887d34f8b9b2d4c032d55c5519599.serveo.net"
+    url: "http://localhost:4500"
+    // url: "https://828887d34f8b9b2d4c032d55c5519599.serveo.net"
 }; 
  
