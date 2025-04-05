820b910c21dc3d6f8dd8886c92ccb96f9b6ad124
Nsanga Mercure Mekinda <mercuremekinda@gmail.com>
2025-04-05 09:38:17 +0100

    fixing error

--- src/components/footer/FooterAdmin.js
+++ src/components/footer/FooterAdmin.js
@@ -38,7 +38,7 @@ export default function Footer() {
         {" "}
         &copy; {1900 + new Date().getYear()}
         <Text as='span' fontWeight='500' ms='4px'>
-          ECOBANK. All Rights Reserved.
+          All Rights Reserved.
         </Text>
       </Text>
     </Flex>
