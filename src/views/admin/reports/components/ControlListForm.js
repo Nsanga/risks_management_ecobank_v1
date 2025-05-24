d6915661af22e32c8e416c9023ad7c5aa5bb6a28
Mercure Mekinda <mercuremekinda@gmail.com>
2025-05-24 17:07:22 +0100

    fixing KI table

--- src/views/admin/reports/components/ControlListForm.js
+++ src/views/admin/reports/components/ControlListForm.js
@@ -4,20 +4,65 @@ import {
   Button,
   FormControl,
   FormLabel,
-  Input,
-  Textarea,
-  Select,
-  Checkbox,
   Heading,
 } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
+import Select from "react-select";
 
 // Composant pour le formulaire Control List
 const ControlListForm = ({ handleOpenView }) => {
+  const [selectedEntity, setSelectedEntity] = useState(null);
   const [formData, setFormData] = useState({
-  
-  session: "", // ✅ ajout
+    session: "", // ✅ ajout
     entity: "",  // ✅ ajout
   });
+
+  const entities = useSelector(state => state.EntityReducer.entities);
+
+  const sessionOptions = [
+    { value: "mensuel", label: "Mensuelle" },
+    { value: "trimestrielle", label: "Trimestrielle" },
+  ];
+
+  const handleSelectChange = (name, selectedOption) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: selectedOption ? selectedOption.value : null,
+    }));
+
+    if (selectedOption) {
+      setSelectedEntity(selectedOption.fullEntity);
+    }
+  };
+
+  const entitiesOptions = entities?.map((entity, index) => ({
+    key: `${entity._id}-${index}`,
+    value: entity._id,
+    label: `ENT${entity.referenceId} CAM - ${entity.description}`,
+    description: entity.description,
+    fullEntity: entity,
+  }));
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+      maxHeight: "200px", // Définir une hauteur maximale pour le menu
+      overflowY: "auto", // Activer le défilement vertical
+    }),
+    option: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+    }),
+  };
 
   return (
     <VStack spacing={4} align="stretch">
@@ -27,31 +72,36 @@ const ControlListForm = ({ handleOpenView }) => {
 
       <FormControl>
         <FormLabel>Session </FormLabel>
-        <Select
-          value={formData.session}
-          onChange={(e) =>
-            setFormData({ ...formData, session: e.target.value })
-          }
-          placeholder="session"
-        >
-          <option value="entity">1: Data for selected Entity</option>
-          <option value="owner">2: Data for selected Owner</option>
-          <option value="all">3: All Data</option>
-        </Select>
+        <Box w="100%">
+          <Select
+            options={sessionOptions}
+            styles={customStyles}
+            placeholder="Select session"
+            value={sessionOptions?.find(
+              (option) => option.value === formData.session
+            )}
+            onChange={(selectedOption) =>
+              handleSelectChange("session", selectedOption)
+            }
+          />
+        </Box>
       </FormControl>
 
       <FormControl>
         <FormLabel>Select Entity</FormLabel>
-        <Select
-          value={formData.entity}
-          onChange={(e) =>
-            setFormData({ ...formData, entity: e.target.value })
-          }
-          placeholder="Select Entity"
-        >
-          <option value="ENT00409">ENT00409 ECM-FICC</option>
-          <option value="ENT00510">ENT00510 ECM-EUR</option>
-        </Select>
+        <Box w="100%">
+          <Select
+            options={entitiesOptions}
+            styles={customStyles}
+            placeholder="Select Entity"
+            value={entitiesOptions?.find(
+              (ent) => ent.value === formData.entity
+            )}
+            onChange={(selectedOption) =>
+              handleSelectChange("entity", selectedOption)
+            }
+          />
+        </Box>
       </FormControl>
 
       <Button onClick={handleOpenView} colorScheme="blue" size="lg">
--- src/views/admin/reports/components/KeyIndicatorAnalysisForm.js
+++ src/views/admin/reports/components/KeyIndicatorAnalysisForm.js
@@ -1,45 +1,83 @@
 import React, { useState } from 'react';
 import {
   VStack,
-  HStack,
   Button,
   FormControl,
   FormLabel,
-  Input,
-  Textarea,
-  Select,
-  NumberInput,
-  NumberInputField,
   Heading,
 } from '@chakra-ui/react';
+import Select from "react-select";
 
 // Composant pour le formulaire Key Indicator Analysis
-const KeyIndicatorAnalysisForm = ({handleViewReport}) => {
-   const [formData, setFormData] = useState({
-    
+const KeyIndicatorAnalysisForm = ({ handleViewReport }) => {
+  const [formData, setFormData] = useState({
     session: "", // ✅ ajout
-      entity: "",  // ✅ ajout
-    });
+    entity: "",  // ✅ ajout
+  });
+  const [selectedEntity, setSelectedEntity] = useState(null);
+
+  const entities = useSelector(state => state.EntityReducer.entities);
+
+  const handleSelectChange = (name, selectedOption) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: selectedOption ? selectedOption.value : null,
+    }));
+
+    if (selectedOption) {
+      setSelectedEntity(selectedOption.fullEntity);
+    }
+  };
+
+  const entitiesOptions = entities?.map((entity, index) => ({
+    key: `${entity._id}-${index}`,
+    value: entity._id,
+    label: `ENT${entity.referenceId} CAM - ${entity.description}`,
+    description: entity.description,
+    fullEntity: entity,
+  }));
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+    }),
+    menu: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+      maxHeight: "200px", // Définir une hauteur maximale pour le menu
+      overflowY: "auto", // Activer le défilement vertical
+    }),
+    option: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      fontSize: "12px",
+    }),
+  };
 
   return (
-     <VStack spacing={4} align="stretch">
-       <Heading size="md" color="blue.600">
-         key indicator List Form
-       </Heading>
- 
-       <FormControl>
-         <FormLabel>Select Entity</FormLabel>
-         <Select
-           value={formData.entity}
-           onChange={(e) =>
-             setFormData({ ...formData, entity: e.target.value })
-           }
-           placeholder="Select Entity"
-         >
-           <option value="ENT00409">ENT00409 ECM-FICC</option>
-           <option value="ENT00510">ENT00510 ECM-EUR</option>
-         </Select>
-       </FormControl>
+    <VStack spacing={4} align="stretch">
+      <Heading size="md" color="blue.600">
+        key indicator List Form
+      </Heading>
+
+      <FormControl>
+        <FormLabel>Select Entity</FormLabel>
+        <Select
+          options={entitiesOptions}
+          styles={customStyles}
+          placeholder="Select Entity"
+          value={entitiesOptions?.find(
+            (ent) => ent.value === formData.entity
+          )}
+          onChange={(selectedOption) =>
+            handleSelectChange("entity", selectedOption)
+          }
+        />
+      </FormControl>
 
       <Button colorScheme="green" size="lg" onClick={handleViewReport} >
         View report
