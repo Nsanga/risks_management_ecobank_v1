c8b467f64ec4192d2227ee33a56c2ea236bc254f
FOKOU DEFO Roberto <robertofokou4@gmail.com>
2025-06-23 16:07:54 +0100

    suite du taf

--- src/views/admin/reports/components/IncidentLossReportForm.js
+++ src/views/admin/reports/components/IncidentLossReportForm.js
@@ -6,20 +6,32 @@ import {
   FormLabel,
   Heading,
   Box,
+  Flex,
+  border,
+  Input,
 } from "@chakra-ui/react";
 import Select from "react-select";
 import MultiSelectCheckbox from "./MultipleSelectCustom";
 
 // Composant pour le formulaire Incident Loss Report
-const IncidentLossReportForm = ({ handleOpenView, entities, loading, onSelectionChange }) => {
+const IncidentLossReportForm = ({
+  handleOpenView,
+  entities,
+  loading,
+  onSelectionChange,
+}) => {
   const [selectedEntity, setSelectedEntity] = useState(null);
   const [formData, setFormData] = useState({
     entity: [],
   });
 
   const handleSelectChange = (name, selectedOption) => {
-    const selectedValues = selectedOption ? selectedOption.map(option => option.value) : [];
-    const selectedEntities = selectedOption ? selectedOption.map(option => option.fullEntity) : [];
+    const selectedValues = selectedOption
+      ? selectedOption.map((option) => option.value)
+      : [];
+    const selectedEntities = selectedOption
+      ? selectedOption.map((option) => option.fullEntity)
+      : [];
 
     setSelectedEntity(selectedEntities);
 
@@ -70,18 +82,46 @@ const IncidentLossReportForm = ({ handleOpenView, entities, loading, onSelection
   return (
     <VStack spacing={4} align="stretch">
       <Heading size="md" color="blue.600">
-        Incident Loss Report 
+        Incident Loss Report
       </Heading>
 
-      <FormControl>
-        <FormLabel>Select Entity</FormLabel>
-        <Box w="100%">
-        <MultiSelectCheckbox
-            name="entity"
-            options={entitiesOptions}
-            value={formData.entity}
-            onChange={handleSelectChange}
-            placeholder="Select Entity"
+      <FormControl display={"flex"} flexDirection="column" gap={4}>
+        <Box display="flex" alignItems="center" w="100%">
+          <FormLabel mb="0" minW="120px">
+            Select Entity
+          </FormLabel>
+          <Box flex="1">
+            <MultiSelectCheckbox
+              name="entity"
+              options={entitiesOptions}
+              value={formData.entity}
+              onChange={handleSelectChange}
+              placeholder="Select Entity"
+            />
+          </Box>
+        </Box>
+
+        <Box display="flex" alignItems="center" w="100%">
+          <FormLabel mb="0" minW="120px">
+            Start Date
+          </FormLabel>
+          <Input
+            fontSize={12}
+            type="date"
+            name="lastOperation"
+            // value={formData.lastOperation}
+          />
+        </Box>
+
+        <Box display="flex" alignItems="center" w="100%">
+          <FormLabel mb="0" minW="120px">
+            Start Date
+          </FormLabel>
+          <Input
+            fontSize={12}
+            type="date"
+            name="lastOperation"
+            // value={formData.lastOperation}
           />
         </Box>
       </FormControl>
