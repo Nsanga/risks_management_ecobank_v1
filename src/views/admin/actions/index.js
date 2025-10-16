710c7c40153a64a3869159e4c00b707f7e5a73a3
Mercure Mekinda <mercuremekinda@gmail.com>
2025-10-16 17:41:57 +0100

    fixing update

--- src/views/admin/actions/index.js
+++ src/views/admin/actions/index.js
@@ -1,37 +1,66 @@
-import { Box, Flex, Image } from '@chakra-ui/react';
-import React, { useEffect } from 'react'
+import { Box, Flex, Image, Radio, RadioGroup, Stack } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import { listActions } from 'redux/actions/action';
 import ActionCard from '../control/components/ActionCard';
-import Loader from "../../../assets/img/loader.gif";
+import Loader from '../../../assets/img/loader.gif';
 
 const Actions = ({ actions, loading }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
+  const [filter, setFilter] = useState('Tous'); // Default filter: show all actions
+  const [filteredActions, setFilteredActions] = useState(actions);
 
-    useEffect(() => {
-        dispatch(listActions());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(listActions());
+  }, [dispatch]);
 
-    console.log("actions::", actions); // Cela devrait maintenant afficher uniquement le tableau d'actions
+  useEffect(() => {
+    // Filter actions based on selected filter
+    if (filter === 'Tous') {
+      setFilteredActions(actions);
+    } else {
+      setFilteredActions(actions.filter(action => action.category === filter));
+    }
+  }, [filter, actions]);
 
-    return (
-        <Box mt={4}>
-            {loading ? (
-                <Flex alignItems="center" justifyContent="center" mt={24}>
-                    <Image src={Loader} alt="loading..." height={50} width={50} />
-                </Flex>
-            ) : (
-                <Box mt={24}>
-                    <ActionCard actions={actions} />
-                </Box>
-            )}
+//   console.log('actions:', actions);
+//   console.log('filteredActions:', filteredActions);
+
+  return (
+    <Box mt={4}>
+      {/* Radio Button Filters */}
+      <Flex justifyContent="end" mb={6} mt={24}>
+        <RadioGroup onChange={setFilter} value={filter}>
+          <Stack direction="row" spacing={4}>
+            <Radio value="Tous" colorScheme="purple">
+              Tous
+            </Radio>
+            <Radio value="RCSA" colorScheme="purple">
+              RCSA
+            </Radio>
+            <Radio value="KRI" colorScheme="purple">
+              KRI
+            </Radio>
+          </Stack>
+        </RadioGroup>
+      </Flex>
+
+      {loading ? (
+        <Flex alignItems="center" justifyContent="center">
+          <Image src={Loader} alt="loading..." height={50} width={50} />
+        </Flex>
+      ) : (
+        <Box>
+          <ActionCard actions={filteredActions} />
         </Box>
-    );
+      )}
+    </Box>
+  );
 };
 
 const mapStateToProps = ({ ActionReducer }) => ({
-    actions: ActionReducer.actions,
-    loading: ActionReducer.loading,
+  actions: ActionReducer.actions,
+  loading: ActionReducer.loading,
 });
 
 export default connect(mapStateToProps)(Actions);
